0f91f067dce715be64f704b89b12a2f2e721610d
Dinu <example@gmail.com>
2023-01-31 20:08:36 +0200

    primele exercitii

--- jS/script.js
+++ jS/script.js
@@ -1,1 +1,115 @@
-console.log("Oleg!!");
+"use strict";
+
+// const num = 20;
+// switch (num) {
+//   case 49:
+//     console.log("minciuni");
+//     break;
+//   case 50:
+//     console.log("nus minciuni");
+//     break;
+//   default:
+//     console.log("ei hai");
+//     break;
+// }
+
+///////////////////////////////////////////////////////// !!! \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+// const fries = 3,
+//   cola = 2;
+
+// if (fries >= 1 && cola >= 1) {
+//   console.log("iuhuu");
+// } else {
+//   console.log("eii");
+// }
+
+///////////////////////////////////////////////////////// !!! \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+// console.log(NaN || 2 || undefined); // 2
+
+// console.log(NaN && 2 && undefined); // NaN
+
+// console.log(1 && 2 && 3); // 3
+
+// console.log((!1 && 2) || !3); // false
+
+// console.log(25 || (null && !3)); // 25
+
+// console.log(NaN || null || !3 || undefined || 5); // 5
+
+// console.log(NaN || (null && !3 && undefined) || 5); //5
+
+// console.log((5 === 5 && 3 > 1) || 5); // true
+
+///////////////////////////////////////////////////////// !!! \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+// for (let i = 1; i < 8; i++) {
+//     if (i === 4) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+///////////////////////////////////////////////////////// !!! \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+
+// let result = "";
+// const theLength = 7;
+
+// for (let i = 1; i < theLength; i++) {
+//   for (let j = 0; j < i; j++) {
+//     result += "*";
+//   }
+
+//   result += "\n";
+// }
+
+// console.log(result);
+
+///////////////////////////////////////////////////////// !!! \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+// Primul: for (let i = 0; i < 3; i++) {
+//   console.log(`Primul nivel ${i}`);
+//   for (let j = 0; j < 3; j++) {
+//     console.log(`Al doilea nivel ${j}`);
+//     for (let k = 0; k < 5; k++) {
+//       console.log(`Al treilea nivel ${k}`);
+//       if (k === 3) {
+//         break Primul;
+//       }
+//     }
+//   }
+// }
+
+///////////////////////////////////////////////////////// !!! \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+// const lines = 5;
+// let result = '';
+
+// for (let i = 0; i <= lines; i++) {
+//     for (let j = 0; j < lines - i; j++) {
+//         result += " ";
+//     }
+//     for (let j = 0; j < 2 * i + 1; j++) {
+//         result += "*";
+//     }
+//     result += "\n";
+// }
+
+// console.log(result);
+
+//      *
+//     ***
+//    *****
+//   *******
+//  *********
+// ***********
+
+///////////////////////////////////////////////////////// !!! \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
